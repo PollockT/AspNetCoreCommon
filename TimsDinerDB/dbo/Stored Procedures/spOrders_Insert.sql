e5ff1d61487eddd6bece9a31d0c84a1ab60ed8ee
Theodore Pollock <pollock86@yahoo.com>
2020-11-15 19:06:03 -0600

    DB for small resturante filled

---
+++ TimsDinerDB/dbo/Stored Procedures/spOrders_Insert.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spOrders_Insert]
+	@OrderName NVARCHAR(50),
+	@OrderDate DATETIME2(7),
+	@FoodId INT,
+	@Quantity INT,
+	@Total MONEY,
+	@Id INT OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO dbo.[Order](OrderName, OrderDate, FoodId, Quantity, Total)
+		VALUES(@OrderName, @OrderDate, @FoodId, @Quantity, @Total);
+
+		set @Id = SCOPE_IDENTITY();
+END
---
+++ TimsDinerDB/dbo/Tables/Order.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Order]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[OrderName] NVARCHAR(50) NOT NULL,
+	[OrderDate] DATETIME2 NOT NULL,
+	[FoodId] INT NULL,
+	[Quantity] INT NOT NULL,
+	[Total] MONEY NOT NULL, 
+    CONSTRAINT [FK_Order_ToFoodId] FOREIGN KEY (FoodId) REFERENCES [dbo].[Food]([Id]),
+
+)
